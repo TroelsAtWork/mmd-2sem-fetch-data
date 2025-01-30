cc741dba02f3dfef2123ed390af4a778eb913ba7
TroelsAtWork <tras@kea.dk>
2025-01-30 06:44:52 +0100

    Fetch dynamic API data to produkt.html

--- produkt.js
+++ produkt.js
@@ -1,1 +1,26 @@
-true;
+const productContainer = document.querySelector(".productContainer");
+const productId = 1530;
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    let { discount, articletype, category, price } = data;
+    productContainer.innerHTML = `
+    <figure>
+        <img
+          src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
+          alt="Produktbillede"
+          class="productImage"
+        />
+        <span class="saleLabel hidden ${discount && "visible"}">Udsalg!</span>
+      </figure>
+      <section class="productDetails">
+        <h2 class="productName">Produktnavn</h2>
+        <div>
+        <p class="articleType"><span class="bold">Type:</span> ${articletype}</p>
+          <p class="productCategory"><span class="bold">Kategori:</span> ${category}</p>
+          <p class="productPrice"><span class="bold">Pris:</span> ${price},-</p>
+        </div>
+        <button class="buyButton">Køb nu</button>
+      </section>
+    `;
+  });
